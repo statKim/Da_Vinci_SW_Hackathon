8949a35e9509510b622fdb5e716569645e15e192
statKim <hyunsung1021@gmail.com>
2018-11-21 22:11:53 +0900

    board

--- app.py
+++ app.py
@@ -2,30 +2,15 @@
 
 app = Flask(__name__)
 
-
-@app.route("/")  # 기본 root 페이지
+# root page
+@app.route("/")
 def hello():
-    return "hello"
-
-
-# root 페이지인 "http://127.0.0.1:5000/" 뒤에 단어를 입력하면 페이지에 출력됩니다.
-# 예를 들어, "http://127.0.0.1:5000/다섯명" => 페이지에 "hello 다섯명" 출력
-@app.route("/<name>")
-def john(name):
-    return "hello {}".format(name)  # "hello %s" % name  과 같은 결과입니다!!
-
-
-# "http://127.0.0.1:5000/search"를 주소창에 입력하면
-# "templates" 폴더 안의 "search.html"이 페이지로 출력됩니다.
-# 그래서 render_template() 함수는 페이지를 이동시켜서 화면을 바꿀 때 사용합니다.
-@app.route("/search")
-def search():
-    return render_template("search.html")
-
-
-@app.route("/test")
-def test():
     return render_template("index.html")
+
+# 게시판 page
+@app.route("/board")
+def board():
+    return render_template("board.html")
 
 
 if __name__ == '__main__':
