48feeda7e5a31ba1fc1d320f382127784d7cf12b
statKim <hyunsung1021@gmail.com>
2018-11-22 16:02:18 +0900

    daum map

--- test.py
+++ test.py
@@ -1,22 +1,18 @@
-# from datetime import datetime
-
-# datetime.today()            # 현재 날짜 가져오기
-# datetime.today().year        # 현재 연도 가져오기
-# datetime.today().month      # 현재 월 가져오기
-# datetime.today().day        # 현재 일 가져오기
-# datetime.today().hour        # 현재 시간 가져오기
-
-# print(datetime.today(), datetime.today().year, datetime.today().month, datetime.today().day, datetime.today().hour+9)
-
-# print(datetime.today().strftime("%Y%m%d%H%M%S"))    # YYYYmmddHHMMSS 형태의 시간 출력
-
-# print(datetime.today().strftime("%Y/%m/%d %H:%M:%S"))  # YYYY/mm/dd HH:MM:SS 형태의 시간 출력
-
-
-from datetime import datetime
-now = datetime.now() 
-
-time = str(now.hour+9) + ":" + str(now.minute)
-
-time = now.strftime("%Y.%m.%d") + " " + datetime.strptime(time, "%H:%M").strftime("%H:%M")
-print(time)
+import os
+import sys
+import urllib.request
+client_id = "xvzlWcQv53Mcer5SY3Ai"
+client_secret = "2i7spZzxmf"
+encText = urllib.parse.quote("불정로 6")
+url = "https://openapi.naver.com/v1/map/geocode?query=" + encText # json 결과
+# url = "https://openapi.naver.com/v1/map/geocode.xml?query=" + encText # xml 결과
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id",client_id)
+request.add_header("X-Naver-Client-Secret",client_secret)
+response = urllib.request.urlopen(request)
+rescode = response.getcode()
+if(rescode==200):
+    response_body = response.read()
+    print(response_body.decode('utf-8'))
+else:
+    print("Error Code:" + rescode)
